afcf9b53f59bbd780131253207c116e16e951609
Eduardo Apolinario <curupa@gmail.com>
2012-10-14 14:25:58 -0700

    Removed ruby version from the prompt

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,8 @@ alias reload!='. ~/.zshrc'
 alias grep='grep --color=always'
 alias more='more -R'
 
+# Tmux related aliases
+alias tm='tmux'
+alias tmlist='tm list-sessions'
+alias tmattach='tm attach-session -t '
+
